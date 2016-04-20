8ad418895e8a259df65e94f2de24b8f57a6b639b
alexwine36 <alexwine36@gmail.com>
2016-04-19 21:33:31 -0600

    Added a seed file for Pages data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Page.create([{ title: 'Home', content: '![alt text](http://res.cloudinary.com/awts/image/upload/c_scale,q_80,w_960/v1460888404/DSC00122_tcoau2.jpg "Morris Circus Full")
+
+## AW Technical Solutions
+
+Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling. Fill information in the Pages Controller.
+
+This effect can do some really cool stuff which you will see if I ever get this working
+
+![alt text](http://res.cloudinary.com/awts/image/upload/c_scale,w_960/v1460888674/DSC00082_pkymdm.jpg "Logo Title Text 1")
+
+## Recent Projects
+
+The pictures displayed on this portion of the site are from the most recent project done, The Morris Circus Exhibit. This installation was done at The Museum of International Folk Art in Santa Fe New Mexico.
+
+![alt text](http://res.cloudinary.com/awts/image/upload/c_scale,w_960/v1460888712/DSC00148_qrj66d.jpg "Logo Title Text 1")'},
+{ title: 'About', content: '![alt text](https://pixabay.com/static/uploads/photo/2014/07/27/20/29/landscape-403165_960_720.jpg "Logo Title Text 1")
+## About
+
+Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling. Fill information in the Pages Controller.
+
+This effect can do some really cool stuff which you will see if I ever get this working
+
+![alt text](https://pixabay.com/static/uploads/photo/2014/07/27/20/29/landscape-403165_960_720.jpg "Logo Title Text 1")' },
+{ tite: 'Footer', content: 'The company that does stuff for you' }])
