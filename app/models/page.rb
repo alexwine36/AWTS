db69d9f4372805a764063ff73c27fa2fced8dbfe
alexwine36 <alexwine36@gmail.com>
2016-04-19 21:58:47 -0600

    Changed Pages Index settings

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,7 @@ class Page
   field :title, type: String
   field :content, type: String
 
+  index({ title: 1 }, { unique: true, name: "title_index" })
+
   validates_presence_of :title, :content
 end
