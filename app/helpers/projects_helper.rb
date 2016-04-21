3985dd4b0c4786b190ee60293872e16313d9272c
alexwine36 <alexwine36@gmail.com>
2016-04-20 22:32:34 -0600

    New Goals working

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,15 +4,9 @@ module ProjectsHelper
     pid = date.strftime('%m%d%y')
     pCount = Project.where(created_at: (DateTime.now.beginning_of_month)..(DateTime.now.end_of_month)).count
     pCount += 1
-
     pid = pid.to_s + pCount.to_s.rjust(3,'0')
-
-
     return pid
   end
 
-  def goal_id
-
-  end
 
 end
